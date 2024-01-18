b6ce3ea3f4d2475543ca4ae7997bee3f23403ce7
Jonathan Rivera <arbeyjr@gmail.com>
2024-01-18 15:19:43 -0500

    Ajuste input buscar, boton nuevo registro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
@@ -13,7 +16,20 @@ module.exports = {
     },
     extend: {
       colors: {
-        'main': '#12b0f4',
+        main: '#12b0f4',
+        boton: '#07ac44',
+        green: {
+          '50': '#f0fdf4',
+          '100': '#dcfce7',
+          '200': '#bbf7d0',
+          '300': '#86efac',
+          '400': '#4ade80',
+          '500': '#22c55e',
+          '600': '#16a34a',
+          '700': '#15803d',
+          '800': '#166534',
+          '900': '#14532d',
+        },
       },
     },
   },
