a0548804c344df16495ff05118da4afd417dae12
Jonathan Rivera <arbeyjr@gmail.com>
2024-01-23 22:10:52 -0500

    Instalar libreria chart.js y ajustar grafica V1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   backend: 'http://localhost:3030',
-};
+  imageURL: 'presentation/assets/images',
+}
--- src/presentation/app/modules/home/components/home/home.component.ts
+++ src/presentation/app/modules/home/components/home/home.component.ts
@@ -1,24 +1,85 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import Chart, { ChartType } from 'chart.js/auto';
+import { environment}  from './../../../../../../environments/environment';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatButtonModule
-  ],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
-  constructor(){}
+  public lineChart!: Chart;
+  public barChart!: Chart;
+
+  imagenUrl = `${environment.imageURL}/ima1.jpg`;
+
+  constructor() {}
 
   ngOnInit(): void {
-      console.log('Componente Home');
+    console.log('Componente Home');
+    console.log('valor de imanUrl: ', this.imagenUrl);
+
+    this.loadLineChart();
+    this.loadBoardChart();
   }
 
+  loadLineChart(){
+    const dataLineChart = {
+      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+      datasets: [
+        {
+          label: 'Line Chart',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1,
+        },
+      ],
+    };
 
+    this.lineChart = new Chart('lineChartId', {
+      type: 'line' as ChartType,
+      data: dataLineChart,
+    });
+  }
+
+  loadBoardChart(){
+    const dataBarChart = {
+      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+      datasets: [
+        {
+          label: 'Bar Chart',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+    this.barChart = new Chart('barChartId', {
+      type: 'bar' as ChartType,
+      data: dataBarChart,
+    });
+  }
 }
